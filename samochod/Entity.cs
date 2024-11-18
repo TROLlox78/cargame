d71d3b9ad80a451fb12b706a3a635fb7f8981545
Richard Tirov <trollox.78@gmail.com>
2024-11-18 09:52:28 +0100

    Add project files.

---
+++ samochod/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samochod
+{
+    public class Entity
+    {
+        Vector2 pos, org;
+        Entity()
+        {
+
+        }
+    }
+}
